645c1e53264e61f03db5bea72a3f6335cd90b858
Lamita <sleilatylamita@gmail.com>
2026-06-24 21:26:58 -0700

    fix port

--- backend/index.js
+++ backend/index.js
@@ -187,4 +187,5 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: err.message });
 });
 
-app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+const PORT= process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
